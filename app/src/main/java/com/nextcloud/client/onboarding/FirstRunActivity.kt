b403e74425271a2a01c1a5bbdf76129f20441eef
gatxancom <devel@gatxan.com>
2024-04-02 09:13:49 +0200

    Brabding inicial

--- app/src/main/java/com/nextcloud/client/onboarding/FirstRunActivity.kt
+++ app/src/main/java/com/nextcloud/client/onboarding/FirstRunActivity.kt
@@ -270,7 +270,7 @@ class FirstRunActivity : BaseActivity(), ViewPager.OnPageChangeListener, Injecta
 
         val firstRun: Array<FeatureItem>
             get() = arrayOf(
-                FeatureItem(R.drawable.logo, R.string.first_run_1_text, R.string.empty, true, false),
+                FeatureItem(R.drawable.gatxan_cloud_logo_blanc_v, R.string.first_run_1_text, R.string.empty, true, false),
                 FeatureItem(R.drawable.first_run_files, R.string.first_run_2_text, R.string.empty, true, false),
                 FeatureItem(R.drawable.first_run_groupware, R.string.first_run_3_text, R.string.empty, true, false),
                 FeatureItem(R.drawable.first_run_talk, R.string.first_run_4_text, R.string.empty, true, false)
